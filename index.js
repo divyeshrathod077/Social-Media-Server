2bcba9b9cb73df64c84eb1d62dfd17029bcf20b0
Divyesh Rathod <rathoddivyesh5911@gmail.com>
2026-06-03 21:54:08 +0530

    frontend link

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://social-media-user-eight.vercel.app',
+    'https://social-media-user-tau.vercel.app/',
     process.env.FRONTEND_URL
 ].filter(Boolean);
 
